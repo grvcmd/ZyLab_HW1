eda806fa2573f3f0eea29739108e30453dc7df98
Ken V <ken.valverde@yahoo.com>
2020-06-08 00:18:53 -0500

    solution to ZyLab 1.19

---
+++ ZyLab_2.19.py
@@ -0,0 +1,32 @@
+# PART 1
+lemon_juice = int(input('Enter amount of lemon juice (in cups):' + '\n'))
+water = int(input('Enter amount of water (in cups):' + '\n'))
+agave_nectar = float(input('Enter amount of agave nectar (in cups):' + '\n'))
+servings = int(input('How many servings does this make?' + '\n'))
+
+print('\n' + 'Lemonade ingredients - yields', '{:.2f}'.format(servings), 'servings')
+print('{:.2f}'.format(lemon_juice), 'cup(s) lemon juice')
+print('{:.2f}'.format(water), 'cup(s) water')
+print('{:.2f}'.format(agave_nectar), 'cup(s) agave nectar')
+
+# PART 2
+desired_servings = int(input('\n' + 'How many servings would you like to make?' + '\n'))
+
+lemon_juice = (desired_servings/servings) * lemon_juice
+water = (desired_servings/servings) * water
+agave_nectar = (desired_servings/servings) * agave_nectar
+
+print('\n' + 'Lemonade ingredients - yields', '{:.2f}'.format(desired_servings), 'servings')
+print('{:.2f}'.format(lemon_juice), 'cup(s) lemon juice')
+print('{:.2f}'.format(water), 'cup(s) water')
+print('{:.2f}'.format(agave_nectar), 'cup(s) agave nectar')
+
+# PART 3
+lemon_juice = lemon_juice/16
+water = water/16
+agave_nectar = agave_nectar/16
+
+print('\n' + 'Lemonade ingredients - yields', '{:.2f}'.format(desired_servings), 'servings')
+print('{:.2f}'.format(lemon_juice), 'gallon(s) lemon juice')
+print('{:.2f}'.format(water), 'gallon(s) water')
+print('{:.2f}'.format(agave_nectar), 'gallon(s) agave nectar')
