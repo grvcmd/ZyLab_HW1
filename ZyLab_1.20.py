e00fb8d62f5fff484ba4adcff2ec3ff54aa39046
Ken V <ken.valverde@yahoo.com>
2020-06-07 23:01:42 -0500

    solution to ZyLab 1.20

--- ZyLab_1.20.py
+++ ZyLab_1.20.py
@@ -1,14 +1,12 @@
-user_num1 = int(input('Enter a number: '))
-print('you entered:', user_num1, '\n')
+user_num1 = int(input('Enter integer:' + '\n'))
+print('You entered:', user_num1)
 
 user_num1_squared = user_num1 * user_num1
-print(user_num1, 'squared is:', user_num1_squared, '\n')
+print(user_num1, 'squared is', user_num1_squared)
 
 user_num1_cubed = user_num1_squared * user_num1
-print(user_num1, 'cubed is:', user_num1_cubed, '\n')
+print('And', user_num1, 'cubed is', user_num1_cubed, '!!')
 
-user_num2 = int(input('Enter another integer: '))
-print('2nd number entered:', user_num2)
-
-print(user_num1, '+', user_num2, 'is', (user_num1 + user_num2))
+user_num2 = int(input('Enter another integer:' + '\n'))
 print(user_num1, '*', user_num2, 'is', (user_num1 * user_num2))
+print(user_num1, '+', user_num2, 'is', (user_num1 + user_num2))
